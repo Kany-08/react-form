b7acf50ef0f787dd528df81bdb5a30e3b6e41d28
kany <kanysharapova@gmail.com>
2025-07-11 12:26:06 +0200

    feat modal text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import { FeedbackForm } from "./components/FeedbackForm/FeedbackForm";
+import { Modal } from "./components/Modal/Modal";
 
 function App() {
+  const [form, setForm] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const onSubmit = (data) => {
+    setForm(data);
+    setShowModal(true);
+  };
   return (
     <>
-      <FeedbackForm />
+      <FeedbackForm onSubmit={onSubmit} />
+      {form && (
+        <Modal title="Спасибо что пишите!" show={showModal}>
+          {form.username}, благодарим Вас за заявку! <br />
+          Мы отправим ответ на вашу почту {form.email}
+        </Modal>
+      )}
     </>
   );
 }
